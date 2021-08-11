279062fe3eaa2cb2a6a982fa2705cff063131946
WMXPY <wm@mengw.io>
2021-08-11 16:26:29 -0500

    init code and ci configf

---
+++ test/unit/placeholder.test.ts
@@ -0,0 +1,16 @@
+/**
+ * @author WMXPY
+ * @namespace HugeInteger
+ * @description Placeholder
+ * @override Unit
+ */
+
+import { expect } from 'chai';
+
+describe('Placeholder', (): void => {
+
+    it('Placeholder', (): void => {
+
+        expect(1).to.be.equal(1);
+    });
+});
